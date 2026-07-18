99895ae041746469cc9a7b16f69b0fbee4dfd829
noahbergmann07-cyber <noahbergmann07@gmail.com>
2026-07-18 18:37:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,45 @@ const elements = document.querySelectorAll(
     ".card, .step, .project, .benefit, .faqBox, .priceCard, .aboutBox"
 );
 
+
 const observer = new IntersectionObserver((entries) => {
 
-    entries.forEach(entry => {
+    entries.forEach((entry, index) => {
 
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
+        if(entry.isIntersecting){
+
+            setTimeout(() => {
+
+                entry.target.classList.add("show");
+
+            }, index * 120);
+
+            observer.unobserve(entry.target);
+
         }
 
     });
 
+}, {
+    threshold: 0.15
 });
 
 
 elements.forEach(element => {
     observer.observe(element);
+});
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+
+        nav.classList.add("scrolled");
+
+    } else {
+
+        nav.classList.remove("scrolled");
+
+    }
+
 });
